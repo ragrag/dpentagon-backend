1fc70de7f3333c54c225acae0b66b3b5829e1811
Raggi <35541698+ragrag@users.noreply.github.com>
2020-12-30 01:06:57 +0200

    Update error.middleware.ts

--- src/api/middlewares/error.middleware.ts
+++ src/api/middlewares/error.middleware.ts
@@ -6,6 +6,7 @@ import { logger } from '../../common/utils/logger';
 function errorMiddleware(error: Boom | Error, req: Request, res: Response, next: NextFunction) {
   const statusCode: number = isBoom(error) ? error.output.statusCode : 500;
   const errorMessage: string = isBoom(error) ? error.message : 'Something went wrong';
+  logger.error(error);
   logger.error(`StatusCode : ${statusCode}, Message : ${errorMessage}`);
 
   return res.status(statusCode).send(errorMessage);
