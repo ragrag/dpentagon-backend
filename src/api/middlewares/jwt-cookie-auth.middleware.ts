cf40836c912fda2bfc166f975bbf61065a062da2
Raggi <35541698+ragrag@users.noreply.github.com>
2021-01-02 00:36:52 +0200

    setup debugger

--- src/api/middlewares/jwt-cookie-auth.middleware.ts
+++ src/api/middlewares/jwt-cookie-auth.middleware.ts
@@ -3,8 +3,10 @@ import passport from 'passport';
 import './passport';
 
 export default function authenticateWithJwtCookie(req: Request, res: Response, next: NextFunction) {
+  const x = 2;
   passport.authenticate('jwt-cookiecombo', { session: false }, (err, user, info) => {
     if (err) return next(err);
+
     if (!user) {
       res.clearCookie('Authorization');
       return res.status(401).send('unauthorized');
